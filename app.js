275b53fa1e673492e70ac8bc35d452e73d0b36d5
airwin1212 <anna.irwin@worldvision.org.uk>
2021-07-07 16:56:52 +0100

    Tag colour tweaks
    Changed visual emails to purple tag

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Get the modal
+// Start
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
@@ -18,6 +18,8 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
   modal.style.display = "none";
 }
+
+// End
 
 var modal = document.getElementById("myModal");
 
@@ -86,6 +88,26 @@ var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("MHGRem3");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("ImpactMail");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function () {
